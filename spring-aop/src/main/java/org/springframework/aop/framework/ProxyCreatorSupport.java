e087823b1cbac8506eb622b367fe1c5d083b0483
FFYzz <cryptochen95@gmail.com>
2021-09-09 22:30:53 +0800

    代理对象创建基础类

--- spring-aop/src/main/java/org/springframework/aop/framework/ProxyCreatorSupport.java
+++ spring-aop/src/main/java/org/springframework/aop/framework/ProxyCreatorSupport.java
@@ -32,8 +32,15 @@ import org.springframework.util.Assert;
 @SuppressWarnings("serial")
 public class ProxyCreatorSupport extends AdvisedSupport {
 
+	/**
+	 * 持有一个 AopProxyFactory
+	 */
 	private AopProxyFactory aopProxyFactory;
 
+	/**
+	 * 以一个 list 进行保存，所以是一个 FIFO 的数据结构。
+	 * 保存多个 AdvisedSupportListener
+	 */
 	private final List<AdvisedSupportListener> listeners = new LinkedList<>();
 
 	/** Set to true when the first AOP proxy has been created. */
@@ -44,6 +51,7 @@ public class ProxyCreatorSupport extends AdvisedSupport {
 	 * Create a new ProxyCreatorSupport instance.
 	 */
 	public ProxyCreatorSupport() {
+		// 默认使用 DefaultAopProxyFactory
 		this.aopProxyFactory = new DefaultAopProxyFactory();
 	}
 
@@ -53,6 +61,7 @@ public class ProxyCreatorSupport extends AdvisedSupport {
 	 */
 	public ProxyCreatorSupport(AopProxyFactory aopProxyFactory) {
 		Assert.notNull(aopProxyFactory, "AopProxyFactory must not be null");
+		// 也可以指定 AopProxyFactory
 		this.aopProxyFactory = aopProxyFactory;
 	}
 
@@ -95,15 +104,18 @@ public class ProxyCreatorSupport extends AdvisedSupport {
 
 
 	/**
-	 * 创建 AOPProxy
+	 * 创建 AOPProxy，给子类调用，并且不允许子类修改的方法。
 	 *
 	 * Subclasses should call this to get a new AOP proxy. They should <b>not</b>
 	 * create an AOP proxy with {@code this} as an argument.
 	 */
 	protected final synchronized AopProxy createAopProxy() {
+		// 事件相关 AdvisedSupportListener
 		if (!this.active) {
+			// 这里会触发激活事件
 			activate();
 		}
+		// 返回一个 AopProxy 代理对象
 		return getAopProxyFactory().createAopProxy(this);
 	}
 
@@ -119,6 +131,8 @@ public class ProxyCreatorSupport extends AdvisedSupport {
 	}
 
 	/**
+	 * 通常查看其引用可以直到有哪些方法会触发 changed 事件
+	 * <p>
 	 * Propagate advice change event to all AdvisedSupportListeners.
 	 * @see AdvisedSupportListener#adviceChanged
 	 */
@@ -126,7 +140,9 @@ public class ProxyCreatorSupport extends AdvisedSupport {
 	protected void adviceChanged() {
 		super.adviceChanged();
 		synchronized (this) {
+			// 必须激活才能触发 changed 事件
 			if (this.active) {
+				// 遍历调用监听器
 				for (AdvisedSupportListener listener : this.listeners) {
 					listener.adviceChanged(this);
 				}
